19c0dbbe44756d047554a25c43367c9544ac1c5c
Nilanjan Mukherjee14 <nilamu14@in.ibm.com>
2022-07-26 20:43:55 +0530

    logger and exception

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
-
+import sys
+from creditcard.logger import logging
+from creditcard.exception import CreditcardException
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        ##raise CreditcardException(e,sys) from e
+        creditcard = CreditcardException(e,sys)
+        logging.info(creditcard.error_message)    
+        logging.info("We are testing logging module")
+    return "CI CD pipeline has been established."
 
 if __name__=="__main__":
     app.run(debug=True)    
+    
