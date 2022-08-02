55816a233de44d41f1b0b0deb365ab2786724b54
Nilanjan Mukherjee14 <nilamu14@in.ibm.com>
2022-08-02 16:23:56 +0530

    updated

--- creditcard/component/data_ingestion.py
+++ creditcard/component/data_ingestion.py
@@ -22,15 +22,15 @@ class DataIngestion:
         try:
             ## Extraction remote url to download dataset
             download_url = self.data_ingestion_config.dataset_download_url
-
             ## folder location to download file 
             tgz_download_dir = self.data_ingestion_config.tgz_download_dir
-
+            os.makedirs(tgz_download_dir, exist_ok=True)
             creditcard_file_name = os.path.basename(download_url)
 
             tgz_file_path = os.path.join(tgz_download_dir,creditcard_file_name)
             logging.info(f"Downloading file from :[{download_url}] into :[{tgz_file_path}]")
-            urllib.request.urlretrieve(download_url,tgz_file_path)    
+            urllib.request.urlretrieve(download_url,tgz_file_path) 
+            return  tgz_file_path   
 
         except Exception as e:
             raise CreditcardException(e,sys) from e
