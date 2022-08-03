609dbd11e2270c652215760af30d0f0727056d1e
Nilanjan Mukherjee14 <nilamu14@in.ibm.com>
2022-08-03 15:29:31 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+from creditcard.config.configuration import Configuration
 from sklearn.pipeline import Pipeline
 from creditcard.pipeline.pipeline import Pipeline
 from creditcard.exception import CreditcardException
@@ -7,6 +8,8 @@ def main():
     try:
         pipeline=Pipeline()
         pipeline.run_pipeline()
+        data_validation_config = Configuration().get_data_validation_config()
+        print(data_validation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e) 
