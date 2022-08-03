57fcd3635287b114c4b7f016996b0fc9dfad2e43
Nilanjan Mukherjee14 <nilamu14@in.ibm.com>
2022-08-03 18:36:31 +0530

    updated

--- creditcard/pipeline/pipeline.py
+++ creditcard/pipeline/pipeline.py
@@ -1,3 +1,4 @@
+from creditcard.component.data_validation import DataValidation
 from creditcard.config.configuration import Configuration
 from creditcard.logger import logging
 from creditcard.exception import CreditcardException
@@ -24,7 +25,14 @@ class Pipeline:
         except Exception as e:
             raise CreditcardException(e,sys) from e
 
-    def start_data_validation(self):
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
+        try:
+            data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),
+                                             data_ingestion_artifact=data_ingestion_artifact
+                                             )
+            return data_validation.initiate_data_validation()
+        except Exception as e:
+            raise CreditcardException(e, sys) from e
         pass
 
     def start_data_transformation(self):
@@ -43,5 +51,6 @@ class Pipeline:
         try:
             ## data ingestion 
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise CreditcardException(e,sys) from e           
